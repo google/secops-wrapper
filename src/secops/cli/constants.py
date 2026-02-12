c6ab4a0daad0240a74e55a653510c90696f3e130
Dan Dye <dandye@google.com>
2026-02-12 15:51:50 -0500

    feat(cli): add SECOPS_LOCAL_CONFIG_DIR env var support

--- src/secops/cli/constants.py
+++ src/secops/cli/constants.py
@@ -7,6 +7,15 @@ from pathlib import Path
 CONFIG_DIR = Path.home() / ".secops"
 CONFIG_FILE = CONFIG_DIR / "config.json"
 
-# Local config (current working directory)
-LOCAL_CONFIG_DIR = Path.cwd() / ".secops"
+import os
+
+# Local config (current working directory or from env var)
+# If SECOPS_LOCAL_CONFIG_DIR is set, use it.
+# Otherwise, default to current working directory + .secops
+_local_config_env = os.environ.get("SECOPS_LOCAL_CONFIG_DIR")
+if _local_config_env:
+    LOCAL_CONFIG_DIR = Path(_local_config_env)
+else:
+    LOCAL_CONFIG_DIR = Path.cwd() / ".secops"
+
 LOCAL_CONFIG_FILE = LOCAL_CONFIG_DIR / "config.json"
